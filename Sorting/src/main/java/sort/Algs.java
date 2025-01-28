be44d7a61abea261757f0bd985212e5576dede70
zthompson2350 <zthompson2350@gmail.com>
2025-01-28 07:48:57 -0600

    Added Selection Sort, Removed Unnecessary Initializations

--- Sorting/src/main/java/sort/Algs.java
+++ Sorting/src/main/java/sort/Algs.java
@@ -3,22 +3,48 @@ package sort;
 public class Algs {
 
 	public void InsertionSort (int[] arr) {
-		int k = 0;
-		int j = 0;
+		
+		int k;
+		int j;
 		
 		for (int i = 1; i < arr.length; i++) {
+			
 			k = arr[i];
 			j = i-1;
 			
 			while (j >= 0) {
+				
 				if (arr[j] < k) break;
 				arr[j+1] = arr[j--];
+				
 			}
 			
 			arr[j+1] = k;
 		}
 	}
 
-	//testing username/email gitconfigs
+	public void SelectionSort (int[] arr) {
+		
+		int minloc;
+		int min;
+		
+		for (int i = 0; i < arr.length - 1; i++) {		
+			
+			minloc = i;
+			
+			for (int j = i+1; j < arr.length; j++) {
+				
+				if (arr[j] < arr[minloc]) {
+					minloc = j;
+				}
+				
+			}
+			
+			min = arr[minloc];
+			arr[minloc] = arr[i];
+			arr[i] = min;
+			
+		}
+	}
 	
 }
